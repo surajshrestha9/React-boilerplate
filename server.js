0aaf22858ea6f3f0644af750ea84816122d179ab
surajshrestha9 <60499784+surajshrestha9@users.noreply.github.com>
2024-06-07 16:05:58 +0545

    server-json midlleware

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+app.listen(5100, () => {
+  console.log("server running...");
+});
