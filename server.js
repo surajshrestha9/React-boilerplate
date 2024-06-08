682f1750e6ab98b2af70dee822cde4d4d319ce71
surajshrestha9 <60499784+surajshrestha9@users.noreply.github.com>
2024-06-08 23:14:31 +0545

    ser

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+import * as dotenv from "dotenv";
 import express from "express";
 const app = express();
+import morgan from "morgan";
+dotenv.config();
+
+// app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//fetch is a promise->use then
+fetch("https://www.course-api.com/react-useReducer-cart-project")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -11,6 +23,9 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
-app.listen(5100, () => {
-  console.log("server running...");
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}...`);
 });
